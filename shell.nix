ac936a1baf22ef26644715ebb470b4d62a4a8d46
ankurs4 <ankurs4@cisco.com>
2021-07-05 22:52:50 +0200

    slightly better nix shell

--- shell.nix
+++ shell.nix
@@ -9,9 +9,20 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    echo "setup stack dev environment"
+    echo "setup localized and isolated stack,cabal,elm dev environment"
     export STACK_ROOT="$PWD/.stack_global_root"
     export CABAL_DIR="$PWD/.cabal_work"
     export CABAL_CONFIG="$CABAL_DIR/config"
+    export ELM_HOME="$PWD/.elm_home"
+
+    if [ -e $STACK_ROOT/config.yaml ]
+    then
+        echo "stack config already ok"
+    else
+        mkdir -p $STACK_ROOT
+        echo "local-bin-path: \"$STACK_ROOT/_build/bin\"" > $STACK_ROOT/config.yaml
+        echo "created stack global config"
+    fi
+    export PATH=$PATH:"$STACK_ROOT/_build/bin"
   '';
 }
